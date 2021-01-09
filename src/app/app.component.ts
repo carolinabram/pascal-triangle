7780e0306775d67c672c9709cb1e05ac7a54f2c0
Carolina Bracamonte <carolina@gilasw.com>
2021-01-09 16:43:23 -0700

    :construction: Added factorial and pascal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pascal-triangle';
+  result = 0;
+
+  constructor(){
+    this.pascal(9);
+  }
+
+  factorial(num){
+    if(num <= 1){
+      return 1;
+    }
+    let result = num;
+    for(let i=1; i<num ;i++){
+      result= result * (num-i);
+    }
+    return result;
+  }
+
+  pascal(power){ // (a+b)^n     n:power
+   const n = this.factorial(power);
+   let array = [];
+
+   for(let k = 0; k<=power; k++){
+      let res = this.factorial(k)*this.factorial(power-k);
+      array.push(n/res);
+   }
+
+   console.log("Power",power,"Array=>", array);
+  }
+
 }
